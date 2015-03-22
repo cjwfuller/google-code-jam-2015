575250238832ecdb2fb132ef784874e7e11a7f44
Chris Fuller <chris@cjwfuller.com>
2015-03-22 18:40:41 +0000

    speedup min scalar product by 10s for small input

--- practice/min_scalar_product.py
+++ practice/min_scalar_product.py
@@ -10,25 +10,26 @@ permuations of the integers 0..n where n is the length of the two vectors
 
 import itertools
 
+def find_min_scalar_product(vector_length, vector1, vector2):
+    # initialse smallest
+    smallest = 0
+    for i in range(vector_length):
+        smallest = smallest + vector1[i] * vector2[i]
+    indices = list(range(vector_length))
+
+    for perm in itertools.permutations(indices, vector_length):
+        test_smallest = 0
+        for i in range(vector_length):
+            test_smallest = test_smallest + vector1[i] * vector2[perm[i]]
+        if test_smallest < smallest:
+            smallest = test_smallest
+    return smallest
+
 if __name__ == '__main__':
     num_cases = int(input())
     for x in range(num_cases):
         vector_length = int(input())
         vector1 = list(map(int, input().rstrip('\n').split(' ')))
         vector2 = list(map(int, input().rstrip('\n').split(' ')))
-
-        # initialse smallest
-        smallest = 0
-        for i in range(vector_length):
-            smallest = smallest + vector1[i] * vector2[i]
-
-        # find Minimum Scalar Product
-        indices = list(range(vector_length))
-        triples = itertools.permutations(indices, vector_length)
-        for t in triples:
-            test_smallest = 0
-            for i in range(vector_length):
-                test_smallest = test_smallest + vector1[i] * vector2[t[i]]
-            if test_smallest < smallest:
-                smallest = test_smallest
+        smallest = find_min_scalar_product(vector_length, vector1, vector2)
         print("Case #" + str(x + 1) + ": " + str(smallest))
